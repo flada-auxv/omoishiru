163c6348cc5a0515512d1cd5311c4ce9118edcb6
flada-auxv <aseknock@gmail.com>
2015-04-27 01:25:34 +0900

    Use current_user in controller & view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
   private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def set_current_user(user)
+    session[:user_id] = user.id
+  end
 
   def omniauth_hash
     env['omniauth.auth']
