3b285aea6aa3b0b9580d196346fb7c53d09a4a2f
flada-auxv <aseknock@gmail.com>
2016-10-18 23:18:33 +0900

    ゼロから

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
-  provider :withings, ENV['WITHINGS_CONSUMER_KEY'], ENV['WITHINGS_CONSUMER_SECRET']
-end
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root 'welcome#index'
-
-  get '/auth/:provider/callback' => 'sessions#create'
-
-  get '/withings/subscribe' => 'withings_notification#subscribe'
-  match '/api/withings/callback' => 'api/withings#callback', via: %i(get post)
-end
